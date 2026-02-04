6cbd733eabbf004ebfa72f6a4e46d1d7e4c0ffe4
Jason <jason.choo2004@gmail.com>
2026-02-04 16:13:14 +0800

    Auto refresh, fulfilled, opacity (-) in calendar

--- ABS_System/obj/Debug/net10.0/ABS_System.AssemblyInfo.cs
+++ ABS_System/obj/Debug/net10.0/ABS_System.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ABS_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f491ba6e86930fe821690ccf049f46c5378d7d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+251eaa2aaa7776519d2927183d5b31bb2dd42222")]
 [assembly: System.Reflection.AssemblyProductAttribute("ABS_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ABS_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
