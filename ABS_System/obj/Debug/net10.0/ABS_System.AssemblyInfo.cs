a13cb4dfbd79ccefc7d3e78d5fbc9516aad7475e
Jason <jason.choo2004@gmail.com>
2026-02-05 17:05:21 +0800

    Print button and as PDF is added

--- ABS_System/obj/Debug/net10.0/ABS_System.AssemblyInfo.cs
+++ ABS_System/obj/Debug/net10.0/ABS_System.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ABS_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de58cfb33f2279aa3198cdda0efb481028ee0eb7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94c3df4625b88744b81b87ee88454ad620d6f640")]
 [assembly: System.Reflection.AssemblyProductAttribute("ABS_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ABS_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
