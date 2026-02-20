9288efc8f10f97c09a45a7c776487b69eb332817
Jason <jason.choo2004@gmail.com>
2026-02-20 12:35:01 +0800

    Signature pop up :good

--- ABS_System/obj/Debug/net10.0/ABS_System.AssemblyInfo.cs
+++ ABS_System/obj/Debug/net10.0/ABS_System.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ABS_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+548b36738d8d11d7699432e616806dede28cf6b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0374c0342344d16ebfad500f8154eb0c5548aee1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ABS_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ABS_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
